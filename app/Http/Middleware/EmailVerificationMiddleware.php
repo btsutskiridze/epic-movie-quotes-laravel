3033223c37252890fe0bfa1e91b83219bead80fb
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-10 10:32:39 +0400

    fix: email verified middleware

--- app/Http/Middleware/EmailVerificationMiddleware.php
+++ app/Http/Middleware/EmailVerificationMiddleware.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -11,13 +11,12 @@ class EmailVerificationMiddleware
 {
 	public function handle(Request $request, Closure $next): JsonResponse
 	{
-		if (!$request->user() ||
-		($request->user() instanceof MustVerifyEmail &&
-		!$request->user()->hasVerifiedEmail()))
+		$user = User::where('email', $request->email)->first();
+		if (!isset($user->email_verified_at))
 		{
 			return response()->json(['errors'=> [
 				'verification'=> 'email is not verified',
-			]], 404);
+			]], 401);
 		}
 		return $next($request);
 	}
