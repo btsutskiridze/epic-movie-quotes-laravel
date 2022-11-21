6a4994011a903784bd7c9c28e2f861d31505fa9c
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-21 13:17:56 +0400

    chore: remove old jwt package

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject, MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail
 {
 	use HasApiTokens;
 
