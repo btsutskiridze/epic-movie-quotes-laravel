b51d92f4fa376768b679c8c37f052b3d4a9f4822
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-11-29 01:52:30 +0400

    feat: like event after for broadcasting likes

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\AddLikeEvent;
 use App\Models\Like;
 use App\Models\Quote;
+use Illuminate\Http\Request;
 
 class LikesController extends Controller
 {
@@ -13,8 +15,10 @@ class LikesController extends Controller
 		return response()->json(['likable'=>$like ? false : true]);
 	}
 
-	public function like(Quote $quote)
+	public function like(Quote $quote, Request $request)
 	{
+		event(new AddLikeEvent($request->all()));
+
 		$like = Like::where('user_id', jwtUser()->id)->where('quote_id', $quote->id)->first();
 
 		if ($like)
